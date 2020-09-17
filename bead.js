7d5a4f109a8c07e482dd088573baccb553e578b2
svasandani <vasandanishailesh@gmail.com>
2020-09-17 00:16:25 -0400

    Even more work idk

--- bead.js
+++ bead.js
@@ -1,1 +1,32 @@
-let bead 
+let bead = document.querySelector(".bead");
+
+let logo = document.querySelector(".logo");
+
+logo.addEventListener('mouseover', () => {
+    bead.classList.add("on-logo");
+})
+
+logo.addEventListener('mouseout', () => {
+    bead.classList.remove("on-logo");
+})
+
+let navlinks = document.querySelectorAll(".nav-link");
+
+navlinks.forEach((nl, i) => {
+    nl.addEventListener('mouseover', () => {
+        bead.classList.add("on-" + (i + 1));
+    })
+    
+    nl.addEventListener('mouseout', () => {
+        if (bead.classList.contains("on-" + (i + 1))) { bead.classList.remove("on-" + (i + 1)); }
+        if (bead.classList.contains("click-" + (i + 1))) { bead.classList.remove("click-" + (i + 1)); }
+    })
+
+    nl.addEventListener('mousedown', () => {
+        bead.classList.add("click-" + (i + 1));
+    })
+
+    nl.addEventListener('mouseup', () => {
+        bead.classList.remove("click-" + (i + 1));
+    })
+})
--- landingcarousel.js
+++ landingcarousel.js
@@ -3,15 +3,44 @@ let landing = document.querySelector("section.landing-section");
 let slides = document.querySelectorAll(".landing-carousel-slide");
 
 let total = slides.length;
-let active = 0;
+let active = -1;
 
-setTimeout(changeSlide, 10000);
+changeSlide();
+
+var timeout = setTimeout(() => {}, 10000);
 
 function changeSlide() {
-    slides[active].style.opacity = "0";
-    console.log(active);
+    let i = active;
     active = active + 1 == total ? 0 : ++active;
-    console.log(active);
-    slides[active].style.opacity = "1";
-    setTimeout(changeSlide, 10000);
+    doSlideChange(i, active);
+    
+    timeout = setTimeout(changeSlide, 10000);
 }
+
+function doSlideChange(i, j) {
+    if (i >= 0) slides[i].style.opacity = "0";
+    slides[j].style.opacity = "1";
+}
+
+let next = document.querySelector(".landing-section .next-slide");
+let prev = document.querySelector(".landing-section .prev-slide");
+
+next.addEventListener('click', () => {
+    if (timeout) clearTimeout(timeout);
+
+    let i = active;
+    active = active + 1 == total ? 0 : ++active;
+    doSlideChange(i, active);
+    
+    timeout = setTimeout(changeSlide, 10000);
+})
+
+prev.addEventListener('click', () => {
+    if (timeout) clearTimeout(timeout);
+
+    let i = active;
+    active = active == 0 ? total - 1 : --active;
+    doSlideChange(i, active);
+    
+    timeout = setTimeout(changeSlide, 10000);
+})
