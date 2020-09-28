b3bf0f93761961e69ece395c7b26a6f6a03de2d6
svasandani <vasandanishailesh@gmail.com>
2020-09-28 00:10:06 -0400

    Change hollow to svg

--- landingcarousel.js
+++ landingcarousel.js
@@ -29,18 +29,18 @@ function doSlideChange(i, j) {
     if (i >= 0) {
         setTimeout(() => { slides[i].style.opacity = "0" }, 1000);
         setTimeout(() => { slides[i].querySelector(".pull-image").style.opacity = "0"; }, 500);
-        slides[i].querySelector("h2").classList.add("push-left");
+        slides[i].querySelector(".hollow").classList.add("push-left");
         setTimeout(() => {
-            slides[i].querySelector("h2:not(.hollow)").classList.add("push-left");
+            slides[i].querySelector("h2").classList.add("push-left");
             setTimeout(() => { slides[i].querySelector("p").classList.add("push-left"); }, 300)
         }, 300);
     }
     setTimeout(() => { 
         slides[j].style.opacity = "1";
         setTimeout(() => { 
-            slides[j].querySelector("h2").classList.remove("push-left");
+            slides[j].querySelector(".hollow").classList.remove("push-left");
             setTimeout(() => {
-                slides[j].querySelector("h2:not(.hollow)").classList.remove("push-left");
+                slides[j].querySelector("h2").classList.remove("push-left");
                 setTimeout(() => { slides[j].querySelector("p").classList.remove("push-left"); }, 200)
             }, 200);
         }, 500);
